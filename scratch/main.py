29dbcd027823506d56cb1934d031a1bee082d63a
wettestsock <evilsharkeater@gmail.com>
2023-09-26 16:27:06 -0400

    no life

--- scratch/main.py
+++ scratch/main.py
@@ -1,3 +1,4 @@
+
 import numpy as npy
 import matplotlib as plt 
 
@@ -10,6 +11,10 @@ inputs = [1,2,3] # every unique input has unique weight associated with it
 #note: inputs are either actual input layer data or output from the previous hidden layer
 # cant be adjusted 
 # weights and bias have to be calculated using backpropagation and calculating gradient
+# can have neural network with just bias or weights , but most of the time all are there
+
+
+#   bias is done to offset the weight to make it positive
 
 weights = [0.2,0.8,-0.5] 
 bias = 2  #every neuron has its own bias
@@ -33,3 +38,113 @@ layerOutput= [
     ]
 
 print('layer output is:',layerOutput)
+
+# more dynamic way to do this:
+weights = [[0.2,0.8,-0.5,1], 
+           [0.5,-0.91,0.26,-0.5], 
+           [-0.26,-0.27,0.17,0.87]]
+biases = [2,3,0.5]
+
+print('zip list is:',list(zip(weights,biases)))
+
+layer_outputs = []
+for nWeights, nBias in zip(weights, biases): # makes a list of lists each of which has elements from both lists
+    nOutput=0
+    for nInput, weight in zip(inputs, nWeights):
+        nOutput+=nInput*weight
+    nOutput+=nBias
+    layer_outputs.append(nOutput)
+
+print(layer_outputs)
+
+
+outputs= npy.dot(inputs,weights[0])+bias  #dot product of 2 vectors 
+                    #sum of all inputs*weights 
+
+print(outputs)
+weights = [[0.2,0.8,-0.5,1], 
+           [0.5,-0.91,0.26,-0.5], 
+           [-0.26,-0.27,0.17,0.87]]
+biases = [2,3,0.5]
+
+
+outputs= npy.dot(weights,inputs)+biases #weights is not a matrix
+            #matrix always goes before the vector
+            #basically does weights C1 * inputs then weights C2 * inputs so on and so forth
+            
+            #first element passed will determine how return how return value indexed 
+print(outputs)
+
+#convert single sample of inputs to a batch
+
+inputs = npy.array([[1,2,3,2.5],
+          [2,5,-1,2.0],
+          [-1.5,2.7,3.3,-0.8]])
+
+weights = npy.array([[0.2,0.8,-0.5,1], 
+           [0.5,-0.91,0.26,-0.5], 
+           [-0.26,-0.27,0.17,0.87]])
+
+biases = npy.array([2,3,0.5])
+
+
+#weights and biases of the 2nd layer
+weights2 = npy.array([[0.1,-0.14,0.5],  
+           [-0.5,0.12,-0.33], 
+           [-0.44,0.73,-0.13]])
+
+biases2 = npy.array([-1,2,-0.5])
+
+
+layer1_output =npy.dot(inputs, weights.T)+biases  #T is transpose  # bias is added to each row 
+#outputs becomes matrix of rows row size = weights row size & column size = transposed inputs size
+
+layer2_output=npy.dot(layer1_output, weights2.T)+biases2
+
+print(npy.array(layer2_output).round(2))
+
+#---------------------
+
+
+#MOREEEEEEEEEEEEEEEEEEEEEEEEEEE
+
+
+#changing size n number of layers
+
+#WEIGHTS -----
+#weights are usually -1 to 1 
+#small values preferred, if each value is bigger than 1 then it explodes
+#if bigger than 1, try to scale them down so that most are -1 to 1
+
+#---
+
+#biases tend to be 0 initially but change if input neuron is 0
+# ^^^^  then it multiplied by anything will still be 0, thus leading to a dead neuron and eventually a dead network
+
+#---
+
+# "hidden" layer -> layers not in charge of adjusting, NN tweaks that 
+
+#---
+
+#sample data
+# X is the standard in ML for inputs
+#also X_train and X_test
+X = npy.array([[1,2,3,2.5],
+          [2,5,-1,2.0],
+          [-1.5,2.7,3.3,-0.8]]) 
+
+
+
+npy.random.seed(0)
+
+
+
+
+class layer_Dense:
+    def __init__(self):
+        pass
+    def forward(self):
+        pass 
+    
+
