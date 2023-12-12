53ea9eb18dea3b9af7e96a8dbb801b93e8159d44
wettestsock <evilsharkeater@gmail.com>
2023-12-12 14:52:36 -0500

    jldjfgb

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ DO EVERYTHING FROM SCRATCH THEN USE PYTHON
 
 TODO: multiple linear regression from scratch
 TODO: learn backpropagation
+TODO: what
+
 
 '''
 
 #inputs from 3 neurons from previous layer
 inputs = [1,2,3,3.5]
 
+
+#  weights and biases tuned knobs in an attemp to fit data
+
 # weights for each neuron
-weights = [[0.2,0.8,-0.5, 1], [0.5,-0.91,0.26,-0.5] ,[0.26,-0.27,0.17,-0.87] ]
+weights = [[0.2,0.8,-0.5, 1], 
+           [0.5,-0.91,0.26,-0.5],
+           [0.26,-0.27,0.17,-0.87] ]
 
 bias = [2,3,0.5]
 
