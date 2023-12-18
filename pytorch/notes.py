125f04eb17b17fa05dcdb926f69e7dce8aa5991a
wettestsock <evilsharkeater@gmail.com>
2023-12-17 20:05:55 -0500

    sdlfjakfdkf[pds

---
+++ pytorch/notes.py
@@ -0,0 +1,29 @@
+
+
+
+'''
+THE THE PYTORCH
+
+following this course
+https://www.youtube.com/watch?v=V_xro1bcAuA&t=171s
+
+this will take a while
+
+
+machine learning: turning something into convertable numbers
+anything and everything can be converted into numbers
+
+"if you can build a simple rule-based system that 
+doesnt require machine learning, do that."
+ - Rule 1 of google's ML handbook
+
+whats it good for then?
+
+
+
+
+
+
+
+
+'''
