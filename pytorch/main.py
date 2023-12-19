4be90f4e55cd9024304dc9e527f9c51c8aa43e35
wettestsock <evilsharkeater@gmail.com>
2023-12-19 16:48:36 -0500

    real

--- pytorch/main.py
+++ pytorch/main.py
@@ -55,12 +55,34 @@ TENSOR = torch.tensor([[[1,2,3],
 
 # returns list size
 # 1st dimension, 2nd, and 3rd
-print(TENSOR.shape)
+#print(TENSOR.shape)
 
 
 '''
 RANDOM TENSORS
 
 why? 
+neural networks start with tensors full of random numbers and then adjust them to better represent the data
 
+start with random numbers -> look at data -> update random numbers -> look at data -> so on
+
+
+note: size = shape
 '''
+
+#of size 3,4 
+
+# num of elements in each dimension 
+random_tensor = torch.rand(3,4)
+
+
+#print(torch.rand(2,10,10,10))  can generate any dimensions
+
+# common code way of representing images
+random_image_size_tensor = torch.rand(size=(224,224,3)) #height, width, color channels
+#print(random_image_size_tensor.shape, random_image_size_tensor.ndim)
+
+
+#tensor of all zeros in size 3,4  
+zeros = torch.zeros(3,4)
+print(zeros)
