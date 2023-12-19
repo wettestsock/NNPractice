bc316517a50c8bccd8ba9bd8b1dbdc52139deee2
wettestsock <evilsharkeater@gmail.com>
2023-12-19 16:13:12 -0500

    jdflsjfo;sd;

---
+++ pytorch/main.py
@@ -0,0 +1,12 @@
+import torch as t
+import pandas as pd 
+import numpy as npy 
+import matplotlib.pyplot as plt 
+
+
+# IT WORKS
+print(t.__version__)
+
+'''
+hello and welcome to super pytorch super 
+'''
--- pytorch/notes.py
+++ pytorch/notes.py
@@ -17,6 +17,19 @@ anything and everything can be converted into numbers
 doesnt require machine learning, do that."
  - Rule 1 of google's ML handbook
 
+EXTRA RESOURCES
+https://github.com/mrdbourke/pytorch-deep-learning
+
+RESEARCH PAPERS ON ML
+https://paperswithcode.com/
+
+PYTORCH PROJECTS TO CONTRIBUTE ON
+https://github.com/ritchieng/the-incredible-pytorch
 
 
+ TODO: learn XGBoost
+ machine learning not deep learning
+
+
+ following along colab
 '''
