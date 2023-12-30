9f9fc28d87b2046e99bde83df1a607792f1b0f47
wettestsock <evilsharkeater@gmail.com>
2023-12-29 21:30:20 -0500

    nothing of note

--- pytorch/intro.py
+++ pytorch/intro.py
@@ -653,7 +653,7 @@ for epoch in range(epochs):
     # 2. FIND LOSS
     #input , target
     loss = loss_fn(y_pred, Y_train) #finds the loss
-
+    print(loss)
 
     # 3. OPTIMIZER ZERO GRAD
     # if slope is 0, sets it to none
@@ -677,7 +677,6 @@ model_0.eval()
 
 # updated model parameters after steps
 
-print(loss_fn)
 print(model_0.state_dict())
 
 
