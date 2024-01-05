097b3115b4ed00649e8ad79ccd9dcd99593e40dd
wettestsock <evilsharkeater@gmail.com>
2024-01-04 20:03:50 -0500

    nothing of note

--- pytorch/classification.py
+++ pytorch/classification.py
@@ -225,7 +225,7 @@ layers and activation functions are separate
 
 model_0.eval()
 with torch.inference_mode():
-    y_logits = model_0(X_test[:5])
+    y_logits = model_0(X_train[:5])
 
 
 torch.save(model_0, 'models/torch_model2.pt')
